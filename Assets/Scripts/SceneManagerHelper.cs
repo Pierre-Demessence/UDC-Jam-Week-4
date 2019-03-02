424f638bf5c3950b39cac904ce4bd1a7dcae38b5
Pierre Demessence <pierre.demessence@gmail.com>
2019-03-02 21:44:56 +0100

    Reworked the whole folder organization.
    Also delete useless files from the assets.

---
+++ Assets/Scripts/SceneManagerHelper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerHelper : MonoBehaviour
+{
+    public void LoadScene(SceneAsset scene)
+    {
+        SceneManager.LoadScene(scene.name);
+    }
+    
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
